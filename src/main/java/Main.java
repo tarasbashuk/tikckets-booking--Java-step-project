a78034c845a18b00d54bb61b55b2ab3a42b284d0
Rodion Lozovoi <rodionlozovoi@gmail.com>
2019-07-28 13:33:21 +0300

    added class for inputs and ConsoleView to manage application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,31 @@
 import controllers.BookingController;
-import entities.Booking;
-import entities.Passenger;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+import view.ConsoleView;
+import view.PassengerInputs;
 
 public class Main {
     public static void main(String[] args) {
 
-        BookingController bookingMgr = new BookingController();
-        Passenger ps1 = new Passenger("Slava", "Stepanchuk");
-        Passenger ps2 = new Passenger("Nastia", "Stepanchuk");
-        Passenger ps3 = new Passenger("Oleksandr", "Ivanov");
+        BookingController bc = new BookingController();
+//        FlightController bc = new FlightController();
 
-        bookingMgr.makeBooking("MyFamilyTrip", ps1,ps1,ps2,ps1,ps1);
-        bookingMgr.makeBooking("IvanovToAmsterdam", ps3, ps3);
-        bookingMgr.makeBooking("NastyaToStambul", ps2, ps2);
+        PassengerInputs pi = new PassengerInputs();
+        ConsoleView table = new ConsoleView();
+//        table.showMenu();
+//        table.MenuItemReader(pi.getMenuItem());
 
 
-        System.out.println("=================================================================");
-        bookingMgr.save();
+//        BookingController bookingMgr = new BookingController();
+//        Passenger ps1 = new Passenger("Slava", "Stepanchuk");
+//        Passenger ps2 = new Passenger("Nastia", "Stepanchuk");
+//        Passenger ps3 = new Passenger("Oleksandr", "Ivanov");
+//
+//        bookingMgr.makeBooking("MyFamilyTrip", ps1,ps1,ps2,ps1,ps1);
+//        bookingMgr.makeBooking("IvanovToAmsterdam", ps3, ps3);
+//        bookingMgr.makeBooking("NastyaToStambul", ps2, ps2);
+//
+//
+//        System.out.println("=================================================================");
+//        bookingMgr.save();
 
 
     }
--- src/main/java/view/PassengerInputs.java
+++ src/main/java/view/PassengerInputs.java
@@ -1,4 +1,81 @@
 package view;
 
+import java.util.Scanner;
+
 public class PassengerInputs {
+    // validators must be added;
+    private Scanner scan = new Scanner(System.in);
+
+    public int getMenuItem() {
+        System.out.println("Enter number of menu item to navigate through the application");
+        int item = scan.nextInt();
+        return item;
+    }
+
+
+    public String getDestination() {
+        System.out.println("Enter your place of destination");
+        String place = scan.nextLine();
+        return validateDestination(place);
+    }
+
+    public String getDate() {
+        System.out.println("Enter date of your flight");
+        String date = scan.nextLine();
+        return validateDate(date);
+    }
+
+
+    public int getTicketsQuantity() {
+        System.out.println("Enter how many people are going to take a flight? ");
+        int tickets = scan.nextInt();
+        return validateTicketsQuantity(tickets);
+    }
+
+
+    public String getName() {
+        System.out.println("Enter your name");
+        return scan.nextLine();
+    }
+
+    public String getSurname() {
+        System.out.println("Enter your surname");
+        return scan.nextLine();
+    }
+
+    public String getFlightId() {
+        System.out.println("Enter your flight ID");
+        return scan.nextLine();
+    }
+
+// validators goes here
+    private String validateDestination(String city) {
+        String cityAgain = scan.nextLine();
+        if (city.length() < 2) {
+            return validateDestination(cityAgain);
+        }
+        boolean check = city.matches("^[a-zA-Z]+(?:[\\s-][a-zA-Z]+)*$");
+        if (!check) {
+            System.out.println("You have to enter correct city name");
+        } else {
+            return city;
+        }
+        return validateDestination(cityAgain);
+    }
+
+    private String validateDate(String date) {
+        String dateAgain = scan.nextLine();
+        boolean check = date.matches("([0-9]{2})\\.([0-9]{2})\\.([0-9]{4})");
+        if (!check) {
+            System.out.println("Not valid date. Enter the date at format dd.mm.yyyy");
+        } else {
+            return date;
+        }
+        return validateDate(dateAgain);
+    }
+
+    private int validateTicketsQuantity(int tickets) {
+        return tickets;
+    }
+
 }
