36cd625a61549f9154f3fdb3ccb2c518a5316702
tarasbashuk <clk55amg>
2019-08-04 17:12:40 +0300

    Done randomizer and push 1000 random flights

--- src/main/java/controllers/FlightController.java
+++ src/main/java/controllers/FlightController.java
@@ -52,42 +52,13 @@ public class FlightController {
 // TODO Delete before release
 
     public static void main(String[] args) throws ParseException {
-//        System.out.println("=========CREATING THREE INITIAL FLIGHTS DEV PURPOSE ONLY============");
-
-        // TO FILL Flights.txt
-
-//        Flight lh1 = new Flight("LH123", "kyiv", "san-francisko", "30.08.2019-12:00", 300);
-//        Flight lh2 = new Flight("LH222", "kyiv", "barcelona", "01.08.2019-12:00", 250);
-//        Flight lh3 = new Flight("LH333", "kyiv", "palma", "03.08.2019-12:00", 200);
-//        Flight lh4 = new Flight("LH444", "kyiv", "madrid", "04.08.2019-22:00", 250);
-//        Flight lh5 = new Flight("LH555", "kyiv", "tel-aviv", "04.08.2019-23:00", 200);
-//        Flight lh6 = new Flight("LH666", "kyiv", "new-york", "05.08.2019-12:00", 250);
-//        Flight lh7 = new Flight("LH777", "kyiv", "wroclaw", "06.08.2019-11:00", 200);
-//        Flight lh8 = new Flight("LH888", "kyiv", "praha", "07.08.2019-12:00", 250);
-//        Flight lh9 = new Flight("LH999", "kyiv", "oslo", "07.08.2019-19:00", 200);
-//        FlightDAO flDAO = new FlightDAO();
-//        flDAO.insert(lh1);
-//        flDAO.insert(lh2);
-//        flDAO.insert(lh3);
-//        flDAO.insert(lh4);
-//        flDAO.insert(lh5);
-//        flDAO.insert(lh6);
-//        flDAO.insert(lh7);
-//        flDAO.insert(lh8);
-//        flDAO.insert(lh9);
-//        flDAO.saveData();
-//        FlightService flService = new FlightService();
+        System.out.println("=========CREATING THREE INITIAL FLIGHTS DEV PURPOSE ONLY============");
 
         FlightController flightMgr = new FlightController();
 
         System.out.println(flightMgr.getAllFlights());
 
-        System.out.println(flightMgr.getSuitableFlights("palma", "03.08.2019", 3));
-        System.out.println(flightMgr.getFlightByNumber("LH123"));
+        System.out.println(flightMgr.getSuitableFlights("barcelona", "05.08.2019", 3));
         System.out.println(flightMgr.getNearestFlights());
-        flightMgr.bookeSeats(5, "LH222");
-        flightMgr.returnSeats(3, "LH222");
-        System.out.println(flightMgr.getAllFlights());
-        flightMgr.save();
     }
 }
--- src/main/java/database/FlightsRandomaizer.java
+++ src/main/java/database/FlightsRandomaizer.java
@@ -1,27 +1,218 @@
 package database;
 
 
+import dao.FlightDAO;
 import entities.Flight;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 public class FlightsRandomaizer {
 
-    private final String[] destinations = {"barcelona", "madrid", "palma", "valencia", "sevillia", "girona"};
+    private final String[] destinations = {"barcelona", "palma", "abu dhabi",
+            "abuja",
+            "accra",
+            "addis ababa",
+            "algiers",
+            "amman",
+            "amsterdam",
+            "andorra la vella",
+            "ankara",
+            "antananarivo",
+            "apia",
+            "ashgabat",
+            "asmara",
+            "astana",
+            "asuncion",
+            "athens",
+            "baghdad",
+            "baku",
+            "bamako",
+            "bandar seri begawan",
+            "bangkok",
+            "bangui",
+            "banjul",
+            "basseterre",
+            "beijing",
+            "beirut",
+            "belfast",
+            "belgrade",
+            "belmopan",
+            "berlin",
+            "bern",
+            "bishkek",
+            "bissau",
+            "bogota",
+            "brasilia",
+            "bratislava",
+            "brazzaville",
+            "bridgetown",
+            "brussels",
+            "bucharest",
+            "budapest",
+            "buenos aires",
+            "cairo",
+            "canberra",
+            "caracas",
+            "cardiff",
+            "castries",
+            "cayenne",
+            "chisinau",
+            "colombo",
+            "conakry",
+            "copenhagen",
+            "dakar",
+            "damascus",
+            "dhaka",
+            "dili",
+            "djibouti",
+            "dodoma",
+            "doha",
+            "dublin",
+            "dushanbe",
+            "edinburgh",
+            "freetown",
+            "funafuti",
+            "gaborone",
+            "georgetown",
+            "gitega",
+            "guatemala city",
+            "hanoi",
+            "harare",
+            "havana",
+            "helsinki",
+            "honiara",
+            "islamabad",
+            "jakarta",
+            "juba",
+            "kabul",
+            "kampala",
+            "kathmandu",
+            "khartoum",
+            "kigali",
+            "kingston",
+            "kingstown",
+            "kinshasa",
+            "kuala lumpur",
+            "kuwait city",
+            "la paz",
+            "libreville",
+            "lilongwe",
+            "lima",
+            "lisbon",
+            "ljubljana",
+            "lome",
+            "london",
+            "luanda",
+            "lusaka",
+            "luxembourg",
+            "madrid",
+            "majuro",
+            "malabo",
+            "male",
+            "managua",
+            "manama",
+            "manila",
+            "maputo",
+            "maseru",
+            "mbabana",
+            "melekeok",
+            "mexico city",
+            "minsk",
+            "mogadishu",
+            "monaco",
+            "monrovia",
+            "montevideo",
+            "moroni",
+            "muscat",
+            "ndjamena",
+            "nairobi",
+            "nassau",
+            "nay pyi taw",
+            "new delhi",
+            "niamey",
+            "nicosia",
+            "nouakchott",
+            "nukualofa",
+            "oslo",
+            "ottawa",
+            "ouagadougou",
+            "palikir",
+            "panama city",
+            "paramaribo",
+            "paris",
+            "phnom penh",
+            "podgorica",
+            "port au prince",
+            "port louis",
+            "port moresby",
+            "port of spain",
+            "port vila",
+            "porto novo",
+            "prague",
+            "praia",
+            "pretoria",
+            "bloemfontein",
+            "cape town",
+            "pristina",
+            "pyongyang",
+            "quito",
+            "rabat",
+            "reykjavik",
+            "riga",
+            "riyadh",
+            "rome",
+            "roseau",
+            "saint georges",
+            "saint john",
+            "san jose",
+            "san marino",
+            "san salvador",
+            "sania",
+            "santiago",
+            "santo domingo",
+            "sao tome",
+            "sarajevo",
+            "seoul",
+            "singapore",
+            "skopje",
+            "sofia",
+            "stockholm",
+            "suva",
+            "taipei",
+            "tallinn",
+            "tarawa atoll",
+            "tashkent",
+            "tbilisi",
+            "tegucigalpa",
+            "tehran",
+            "tel aviv ",
+            "thimphu",
+            "tirana",
+            "tokyo",
+            "tripoli",
+            "tunis",
+            "ulaanbaatar",
+            "vaduz",
+            "valletta",
+            "vatican city",
+            "victoria",
+            "vienna",
+            "vientiane",
+            "vilnius",
+            "warsaw",
+            "washington",
+            "wellington",
+            "windhoek",
+            "yamoussoukro",
+            "yaounde",
+            "yerevan",
+            "zagreb"
+    };
     private final int arraySize = destinations.length;
     private final int quantity;
-    private String flightNumber;
-    private final String from = "kyiv";
-    private String date;
-    private int seats;
-    private final int seatsMin = 100;
-    private final int seatsMax = 350;
-    private final int yearMin = 2000;
-    private final int yearMax = 2016;
 
 
     public FlightsRandomaizer(int quantity) {
@@ -44,32 +235,54 @@ public class FlightsRandomaizer {
 
     private String generateFlightNumber(String from, String destination, int id) {
         StringBuilder sb = new StringBuilder();
-        sb.append(from.charAt(0));
-        sb.append(destination.charAt(0));
+        sb.append(from.toUpperCase().charAt(0));
+        sb.append(destination.toUpperCase().charAt(0));
         sb.append(id);
         return sb.toString();
     }
 
     private String generateDate() {
-        StringBuilder sb = new StringBuilder();
+        int day = generateValueFromRange(1, 30);
+        int month = generateValueFromRange(8, 12);
+        String dayString = Integer.toString(day);
+        if (dayString.length() == 1) dayString = "0"+dayString;
+        String monthString = Integer.toString(month);
+        if (monthString.length() == 1) monthString = "0"+monthString;
 
+        int hour = generateValueFromRange(0, 24);
+        StringBuilder sb = new StringBuilder();
+        sb.append(dayString);
+        sb.append(".");
+        sb.append(monthString);
+        sb.append(".");
+        sb.append("2019");
+        sb.append("-");
+        sb.append(hour);
+        sb.append(":");
+        sb.append("00");
         return sb.toString();
     }
 
     private Flight generateNewFlight() throws ParseException {
         int id = generateValueFromRange(100, 1000);
         String destination = destinations[generateValueFromRange(0, arraySize)];
+        int seatsMin = 100;
+        int seatsMax = 350;
         int seats = generateValueFromRange(seatsMin, seatsMax);
-        flightNumber = generateFlightNumber(from, destination, id);
-        int beginYear = generateValueFromRange(yearMin, yearMax);
-        return new Flight(flightNumber, from, destination, "30.08.2019-12:00", seats);
+        String from = "kyiv";
+        String flightNumber = generateFlightNumber(from, destination, id);
+        String date = generateDate();
+        return new Flight(flightNumber, from, destination, date, seats);
     }
 
 
     public static void main(String[] args) throws ParseException {
-        FlightsRandomaizer newFlights = new FlightsRandomaizer(10);
+        FlightsRandomaizer newFlights = new FlightsRandomaizer(1000);
         List<Flight> flights = newFlights.get();
-        System.out.println(flights.toString());
-
+        FlightDAO flDAO = new FlightDAO();
+        for (int i = 0; i < flights.size(); i++) {
+            flDAO.insert(flights.get(i));
+        }
+        flDAO.saveData();
     }
 }
