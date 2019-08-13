7485787cd34ee35d97c845ee2b923f20b57db0c2
tarasbashuk <clk55amg>
2019-08-13 14:22:01 +0300

    add tests

---
+++ src/test/java/controllers/FlightControllerTest.java
@@ -0,0 +1,104 @@
+package controllers;
+
+import dao.FlightDAO;
+import entities.AirTrip;
+import entities.Flight;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import services.FlightService;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightControllerTest {
+    private SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy-HH:mm");
+    private static File file;
+    private static List<Flight> referenceArray = new ArrayList<>();
+    private static FlightController controller;
+    private static FlightDAO flDAO;
+    private static Flight lh1;
+    private static Flight lh2;
+
+    @BeforeAll
+    static void beforeAll() throws ParseException {
+        file = new File("./src/test/data", "flightsTest.txt");
+
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.print("");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        lh1 = new Flight("LH123", "kyiv", "san-francisko", "14.08.2019-12:00", "15.08.2019-02:00", 300);
+        lh2 = new Flight("LH222", "kyiv", "barcelona", "01.08.2019-12:00", "01.08.2019-14:00", 250);
+        flDAO = new FlightDAO(file);
+        flDAO.retrieveInitialData();
+        flDAO.insert(lh1);
+        flDAO.insert(lh2);
+        flDAO.saveData();
+        controller = new FlightController(file);
+
+        referenceArray.add(lh1);
+        referenceArray.add(lh2);
+    }
+
+    @Test
+    void getAllFlights() {
+        List<Flight> result = controller.getAllFlights();
+        assertEquals(2, result.size());
+        Flight testFlight1 = result.get(0);
+        assertEquals(Flight.class, testFlight1.getClass());
+
+        assertTrue(result.contains(lh1));
+        assertTrue(result.contains(lh2));
+    }
+
+    @Test
+    void getSuitableFlights() {
+        List<AirTrip> referenceArray = new ArrayList<>();
+        referenceArray.add(lh2);
+        assertEquals(referenceArray, controller.getSuitableFlights("barcelona", "01.08.2019", 3));
+        assertNotEquals(referenceArray, controller.getSuitableFlights("barcelona", "01.08.2019", 300));
+        assertNotEquals(referenceArray, controller.getSuitableFlights("madrid", "01.08.2019", 3));
+        assertNotEquals(referenceArray, controller.getSuitableFlights("barcelona", "02.08.2019", 300));
+    }
+
+    @Test
+    void getFlightByNumber() {
+        assertEquals(lh1, controller.getFlightByNumber("LH123"));
+    }
+
+
+    //before test change flights date manually
+    @Test
+    void getNearestFlights() {
+        List<AirTrip> referenceArray = new ArrayList<>();
+        referenceArray.add(lh1);
+        assertEquals(referenceArray, controller.getNearestFlights());
+    }
+
+    @Test
+    void bookSeats() {
+        int initialSeats = controller.getFlightByNumber("LH123").getSeats();
+        controller.bookSeats(10, "LH123");
+        assertEquals( initialSeats - 10, controller.getFlightByNumber("LH123").getSeats());
+    }
+
+    @Test
+    void returnSeats() {
+        int initialSeats = controller.getFlightByNumber("LH123").getSeats();
+        controller.returnSeats(10, "LH123");
+        assertEquals( initialSeats + 10, controller.getFlightByNumber("LH123").getSeats());
+    }
+
+}
--- src/test/java/entities/FlightTest.java
+++ src/test/java/entities/FlightTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,20 +17,21 @@ class FlightTest {
 
     @BeforeEach
     void beforeEach() throws ParseException {
-         flight = new Flight("XX222", "kyiv", "barcelona", "05.08.2019-12:00", 100);
-         flight2 = new Flight("YY111", "kyiv", "madrid", "07.08.2019-13:00", 200);
+         flight = new Flight("XX222", "kyiv", "barcelona", "05.08.2019-12:00", "05.08.2019-14:00", 100);
+         flight2 = new Flight("YY111", "kyiv", "madrid", "07.08.2019-13:00", "07.08.2019-15:00", 200);
     }
 
     @Test
     void constructor(){
-        assertThrows(IllegalArgumentException.class, ()->new Flight(null, "barcelona", "kyiv", "05.08.2019-12:00", 20));
-        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", null, "kyiv", "05.08.2019-12:00", 20));
-        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", null, "05.08.2019-12:00", 20));
-        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", "kyiv", null, 20));
-        assertThrows(ParseException.class, ()->new Flight("XX00", "barcelona", "kyiv", "05.08.2019", 20));
-        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", "kyiv", "05.08.2019-12:00", -1));
-
+        assertThrows(IllegalArgumentException.class, ()->new Flight(null, "barcelona", "kyiv", "05.08.2019-12:00", "05.08.2019-13:00", 20));
+        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", null, "kyiv", "05.08.2019-12:00", "05.08.2019-13:00", 20));
+        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", null, "05.08.2019-12:00","05.08.2019-13:00",  20));
+        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", "kyiv", null, "05.08.2019-13:00", 20));
+        assertThrows(ParseException.class, ()->new Flight("XX00", "barcelona", "kyiv", "05.08.2019", "05.08.2019-13:00", 20));
+        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", "kyiv", "05.08.2019-12:00", "05.08.2019-13:00", -1));
+        assertThrows(IllegalArgumentException.class, ()->new Flight("XX00", "barcelona", "kyiv", "05.08.2019-12:00", null, -1));
     }
+
     @Test
     void getFlightNumber() {
         assertEquals("XX222", flight.getFlightNumber());
@@ -40,18 +43,34 @@ class FlightTest {
     }
 
     @Test
-    void getDateString() {
-        assertEquals("05.08.2019", flight.getDateString());
+    void getFrom() {
+        assertEquals("kyiv", flight.getFrom());
+    }
+
+
+    @Test
+    void getDepartString() {
+        assertEquals("05.08.2019", flight.getDepartString());
     }
 
     @Test
-    void getDate() throws ParseException {
-        assertEquals(format.parse("05.08.2019-12:00"), flight.getDate());
+    void getDepart() throws ParseException {
+        assertEquals(format.parse("05.08.2019-12:00"), flight.getDepart());
     }
 
     @Test
-    void getTime() {
-        assertEquals("12:00", flight.getTime());
+    void getArrival() throws ParseException {
+        assertEquals(format.parse("05.08.2019-14:00"), flight.getArrival());
+    }
+
+    @Test
+    void getDepartTime() {
+        assertEquals("12:00", flight.getDepartTime());
+    }
+
+    @Test
+    void getArrivalTime() {
+        assertEquals("14:00", flight.getArrivalTime());
     }
 
     @Test
@@ -60,9 +79,9 @@ class FlightTest {
     }
 
     @Test
-    void bookeSeats() {
+    void bookSeats() {
         int initialSeats = flight.getSeats();
-        flight.bookeSeats(10);
+        flight.bookSeats(10);
         assertEquals( initialSeats - 10, flight.getSeats());
 
     }
@@ -76,12 +95,19 @@ class FlightTest {
 
 
     @Test
+    void getDirectFlights() {
+        List<Flight> referenceArray = new ArrayList<>();
+        referenceArray.add(flight);
+        assertEquals( referenceArray, flight.getDirectFlights());
+    }
+
+    @Test
     void equals() throws ParseException {
         Flight copyFlight = flight;
         assertEquals(copyFlight,flight);
         assertNotEquals(flight, flight2);
         assertNotEquals(null, flight);
-        Flight fullCopy = new Flight("XX222", "kyiv", "barcelona", "05.08.2019-12:00", 100);
+        Flight fullCopy = new Flight("XX222", "kyiv", "barcelona", "05.08.2019-12:00", "05.08.2019-14:00", 100);
         assertEquals(fullCopy, flight);
 
     }
